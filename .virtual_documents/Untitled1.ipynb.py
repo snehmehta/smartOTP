fd3e8e78270ca3ba2ee313a20e4537607d3705cc
Sneh Mehta <snehmehta933@gmail.com>
2020-05-21 10:48:57 +0530

    legit user part

--- .virtual_documents/Untitled1.ipynb.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-
-
-client = pymongo.MongoClient("mongodb+srv://unlikely:<coolperson>@cluster0-5cxq4.mongodb.net/test?retryWrites=true&w=majority")
-
-
-database = client.get_database('')
